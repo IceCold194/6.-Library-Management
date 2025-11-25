2d4e58c6b4a517d9818362737230ba26cab80550
Ice713 <icramzilruiz@gmail.com>
2025-11-25 20:28:23 +0800

    Done with ReferenceBook Class

---
+++ 6. Library Management/ReferenceBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Library_Management
+{
+    internal class ReferenceBook : Book
+    {
+        public string Subject { get; set; }
+
+        public ReferenceBook(string title, string author, string subject) : base(title, author)
+        {
+            Subject = subject;
+        }
+
+        public override string ShowDetails()
+        {
+            return base.ShowDetails() + $"Reference Subject: {Subject}";
+        }
+    }
+}
